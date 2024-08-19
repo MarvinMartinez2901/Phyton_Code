3d42ccdf579f45aa025da59c16a420492873f70c
MarvinMartinez2901 <mc2024256@inti.edu.sv>
2024-08-19 14:17:45 -0600

    Phyton_Code
    Se agregaron 3 archivos al repositorio

---
+++ 01-variables.py
@@ -0,0 +1,36 @@
+#Comentario de Phyton;
+""" Podemos agregar un comentario demas usando los 
+atajos de tecla """;
+
+#Tipos de variables;
+
+# 1: Texto, Cadena, Alfanumerico (String)
+variableTexto = "Hola vienbenido a phyton";
+print(variableTexto);
+
+variableTexto2 = "Aqui hay un texto para concatenar"               
+print(variableTexto, variableTexto2);
+
+print(variableTexto.upper());
+print(variableTexto2, find("t"));
+
+vavriableTexto3 = "AQUI HAY UN TEXTO MAS";
+
+print(vavriableTexto3,lower());
+
+varibleTexto4 = "Texto de reemplazo";
+
+print(varibleTexto4.replace("Text", "Palabra"))
+
+# 2: Numerica (Int, Float, BigInt, etc)
+VaribleNumEntero = 10; 
+variableNUmDecimal = 7.5;
+
+print(VaribleNumEntero, variableNUmDecimal);
+
+# 3: Booplean (Verdadero o Falso)
+
+variableV = True;
+variableF = False;
+
+print(variableV, "o" , variableF);;
---
+++ 02-Operadores.py
@@ -0,0 +1,22 @@
+# Operadores aritmeticos
+
+# 1: Suma 
+print("Suma: ", 5 + 10);
+
+# Alternativas
+print("Suma N Numeros: ", 5 + 10+ 15);
+
+# 2: Resta 
+print("Resta. " 5 - 15);
+
+# 3: Multiplicacion
+print("Multiplicacion : ", 3 * 5);
+
+# 4: Division 
+print("Division: ", 15 / 5);
+
+# 5: Potencia
+print("Potencia : ", 2 ** 3);
+
+# 6: Modulo
+print("Modulo : ", 10 % 3);
